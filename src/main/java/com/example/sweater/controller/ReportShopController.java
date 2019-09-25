356eb3ffa57a58f4441775a8c4c5dc919f803cb2
Alex <alex.usov.job@gmail.com>
2019-09-25 23:54:28 +0300

    add chart

--- src/main/java/com/example/sweater/controller/ReportShopController.java
+++ src/main/java/com/example/sweater/controller/ReportShopController.java
@@ -123,6 +123,65 @@ public class ReportShopController {
         return "reportShopReceipt";
     }
 
+
+    @PostMapping("/reportShopReceiptChart")
+    public String reportShopReceiptChart(@RequestParam String dateFrom, @RequestParam String dateTo,
+                                    @RequestParam String department, Map<String, Object> model) throws ParseException {
+
+        int productCounter = 0;
+        double allCost = 0.00;
+        String currentUserDepartment, correctDepartment;
+        Date dateFromDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateFrom);
+        Date dateToDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateTo);
+        if(department.equals(" - Отобразить все - ")){
+            correctDepartment = "";
+        }else{
+            correctDepartment = department;
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        currentUserDepartment = authenticationInfo.getCurrentUser().getStoreName();
+        Iterable<Receipt> allReceipt;
+        if (authenticationInfo.getCurrentUser().isShowAdmin()) {
+//            allReceipt = receiptRepo.findAllOrderBySaleDateDesc(formatter.format(todayDate));
+            allReceipt = receiptRepo.findAllBySaleDate2AndDepartmentOrderBySaleDateDesc(formatter.format(dateFromDate),
+                    formatter.format(dateToDate), correctDepartment);
+
+            model.putAll(authenticationInfo.getDepartmentList(model));
+        } else {
+            /*  allReceipt = receiptRepo.findAllByAuthorOrderBySaleDateDesc(currentUserName, formatter.format(dateFromDate));*/
+            allReceipt = receiptRepo.findAllBySaleDate2AndDepartmentOrderBySaleDateDesc(formatter.format(dateFromDate),
+                    formatter.format(dateToDate), currentUserDepartment);
+            List<String> departmentList = new ArrayList<>();
+            departmentList.add(authenticationInfo.getCurrentUser().getStoreName());
+            model.put("departmentList", departmentList);
+
+        }
+        ReportShopReceiptFilter reportShopReceiptFilter = new ReportShopReceiptFilter(department, dateFrom, dateTo);
+        reportShopReceiptFilterRepo.save(reportShopReceiptFilter);
+
+        ReportShopReceiptFilter reportShopReceiptFilterRestore = reportShopReceiptFilterRepo.findFirstByOrderById();
+
+        model.put("reportShopReceiptFilterRestore", reportShopReceiptFilterRestore.getDateFrom());
+        model.put("currentUserName", currentUserDepartment);
+
+        model.put("dateFrom", reportShopReceiptFilterRestore.getDateFrom());
+        model.put("dateTo", reportShopReceiptFilterRestore.getDateTo());
+        model.put("department", reportShopReceiptFilterRestore.getDepartment());
+
+        model.put("productCounter", productCounter);
+        model.put("AllCost", allCost);
+        model.put("allReceipt", allReceipt);
+        model.put("currentProductCounter", getCurrentProductCounter(allReceipt));
+        model.put("currentSummCost", getCurrentSummCost(allReceipt));
+
+        model.putAll(authenticationInfo.getPermission(model));
+
+
+
+
+        return "reportShopReceiptChart";
+    }
+
     public int getCurrentProductCounter(Iterable<Receipt> allReceipt) {
         int currentProductCounter = 0;
         for (Receipt receipt : allReceipt) {
